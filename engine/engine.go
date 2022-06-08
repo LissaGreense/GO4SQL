2dee45d108470516b278b15d745569b1553bb63a
ixior462 <pawel.krupski@mail.com>
2022-06-08 23:50:03 +0200

    Add SelectFromTable

--- engine/engine.go
+++ engine/engine.go
@@ -1,18 +1,21 @@
 package engine
 
 import (
+	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/LissaGreense/GO4SQL/ast"
 	"github.com/LissaGreense/GO4SQL/token"
 )
 
 type DbEngine struct {
-	Tables map[string]map[string]*Column
+	Tables map[string]map[int]*Column
 }
 
 type Column struct {
+	Name   string
 	Type   token.Token
 	Values []interface{}
 }
@@ -20,7 +23,7 @@ type Column struct {
 // New Return new DbEngine struct
 func New() *DbEngine {
 	engine := &DbEngine{}
-	engine.Tables = make(map[string]map[string]*Column)
+	engine.Tables = make(map[string]map[int]*Column)
 	return engine
 }
 
@@ -31,12 +34,13 @@ func (engine *DbEngine) CreateTable(command *ast.CreateCommand) {
 		log.Fatal("Table with the name of " + command.Name.Token.Literal + " already exist!")
 	}
 
-	engine.Tables[command.Name.Token.Literal] = make(map[string]*Column)
+	engine.Tables[command.Name.Token.Literal] = make(map[int]*Column)
 
 	for i, columnName := range command.ColumnNames {
 		column := Column{Type: command.ColumnTypes[i]}
 		column.Values = make([]interface{}, 0)
-		engine.Tables[command.Name.Token.Literal][columnName] = &column
+		column.Name = columnName
+		engine.Tables[command.Name.Token.Literal][i] = &column
 	}
 }
 
@@ -52,11 +56,47 @@ func (engine *DbEngine) InsertIntoTable(command *ast.InsertCommand) {
 	}
 
 	i := 0
-	for columnName, columnStruct := range table {
-		if columnStruct.Type.Type != engine.Tables[command.Name.Token.Literal][columnName].Type.Type {
-			log.Fatal("Invalid Token Type in Insert Command, expecting: " + engine.Tables[command.Name.Token.Literal][columnName].Type.Type + ", got: " + columnStruct.Type.Type)
+	for columnName, columnType := range table {
+		if columnType.Type.Type != engine.Tables[command.Name.Token.Literal][columnName].Type.Type {
+			log.Fatal("Invalid Token Type in Insert Command, expecting: " + engine.Tables[command.Name.Token.Literal][columnName].Type.Type + ", got: " + columnType.Type.Type)
 		}
 		engine.Tables[command.Name.Token.Literal][columnName].Values = append(engine.Tables[command.Name.Token.Literal][columnName].Values, command.Values[i].Literal)
 		i++
 	}
+}
+
+func (engine *DbEngine) SelectFromTable(command *ast.SelectCommand) string {
+	table, exist := engine.Tables[command.Name.Token.Literal]
+	result := ""
+
+	if !exist {
+		log.Fatal("Table with the name of " + command.Name.Token.Literal + " doesn't exist!")
+	}
+
+	if command.Space[0].Type == token.ASTERISK {
+		for i := 0; i < len(table); i++ {
+			result += table[i].Name
+			result += "|"
+		}
+		result = strings.TrimSuffix(result, "|")
+		result += "\n"
+
+		rowsCount := len(table[0].Values)
+
+		for iRow := 0; iRow < rowsCount; iRow++ {
+			for iColumn := 0; iColumn < len(table); iColumn++ {
+				if table[iColumn].Type.Literal == token.TEXT {
+					result += "'" + fmt.Sprintf("%v", table[iColumn].Values[iRow]) + "'"
+				} else {
+					result += fmt.Sprintf("%v", table[iColumn].Values[iRow])
+				}
+				result += "|"
+			}
+			result = strings.TrimSuffix(result, "|")
+			result += "\n"
+		}
+		result = strings.TrimSuffix(result, "\n")
+	}
+
+	return result
 }
--- engine/engine_test.go
+++ engine/engine_test.go
@@ -39,29 +39,63 @@ func TestParserCreateCommand(t *testing.T) {
 		t.Error()
 	}
 
-	if engine.Tables["TBL"]["ONE"].Type.Type != "TEXT" {
+	if engine.Tables["TBL"][0].Type.Type != "TEXT" {
 		t.Error()
 	}
-	if engine.Tables["TBL"]["TWO"].Type.Type != "INT" {
+	if engine.Tables["TBL"][1].Type.Type != "INT" {
 		t.Error()
 	}
-	if engine.Tables["TBL"]["ONE"].Values[0] != "HELLO" {
+	if engine.Tables["TBL"][0].Values[0] != "HELLO" {
 		t.Error()
 	}
-	if engine.Tables["TBL"]["ONE"].Values[1] != "GOODBYE" {
+	if engine.Tables["TBL"][0].Values[1] != "GOODBYE" {
 		t.Error()
 	}
-	if engine.Tables["TBL"]["ONE"].Values[2] != "BYEBYE" {
+	if engine.Tables["TBL"][0].Values[2] != "BYEBYE" {
 		t.Error()
 	}
 
-	if engine.Tables["TBL"]["TWO"].Values[0] != "10" {
+	if engine.Tables["TBL"][1].Values[0] != "10" {
 		t.Error()
 	}
-	if engine.Tables["TBL"]["TWO"].Values[1] != "20" {
+	if engine.Tables["TBL"][1].Values[1] != "20" {
 		t.Error()
 	}
-	if engine.Tables["TBL"]["TWO"].Values[2] != "3333" {
+	if engine.Tables["TBL"][1].Values[2] != "3333" {
 		t.Error()
+	}
+}
+
+func TestSelectCommand(t *testing.T) {
+
+	input :=
+		`
+		create table 	tbl( one TEXT , two INT, three INT, four TEXT );
+		insert into tbl 	values( 'hello',	1, 	11, 'q'  );
+		insert 	into tbl  	values( 'goodbye', 	2, 	22, 'w'  );
+		insert 	into tbl  	values( 'byebye', 	3, 	33,	'e'  );
+		select * from tbl;
+		`
+
+	lexer := lexer.RunLexer(input)
+	parserInstance := parser.New(lexer)
+	sequences := parserInstance.ParseSequence()
+
+	if len(sequences.Commands) != 5 {
+		t.Fatalf("sequences does not contain 5 statements. got=%d", len(sequences.Commands))
+	}
+
+	engine := New()
+	engine.CreateTable((sequences.Commands[0]).(*ast.CreateCommand))
+	engine.InsertIntoTable(sequences.Commands[1].(*ast.InsertCommand))
+	engine.InsertIntoTable(sequences.Commands[2].(*ast.InsertCommand))
+	engine.InsertIntoTable(sequences.Commands[3].(*ast.InsertCommand))
+
+	result := engine.SelectFromTable(sequences.Commands[4].(*ast.SelectCommand))
+
+	expectedResult := "ONE|TWO|THREE|FOUR" + "\n" + "'HELLO'|1|11|'Q'" + "\n" + "'GOODBYE'|2|22|'W'" + "\n" + "'BYEBYE'|3|33|'E'"
+
+	if result != expectedResult {
+		t.Error(result)
 	}
 }
