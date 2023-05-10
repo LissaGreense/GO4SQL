0165ddf7ca8d08722373e227ddaceaca82a12637
LissaGreense <walissa13@gmail.com>
2023-05-10 02:07:23 +0200

    Allow whitespaces in IDENT between apostrophes
    update lexer logic, allow numbers at the beggining of IDENT

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,18 +1,20 @@
 package lexer
 
 import (
+	"bytes"
 	"github.com/LissaGreense/GO4SQL/token"
 )
 
 type Lexer struct {
-	input        string
-	position     int  // current position in input (points to current char)
-	readPosition int  // current reading position in input (after current char)
-	character    byte // current char under examination
+	input             string
+	position          int  // current position in input (points to current char)
+	readPosition      int  // current reading position in input (after current char)
+	character         byte // current char under examination
+	insideApostrophes bool // flag which tells if lexer is between apostrophes
 }
 
 func RunLexer(input string) *Lexer {
-	l := &Lexer{input: input}
+	l := &Lexer{input: input, insideApostrophes: false}
 	l.readChar()
 	return l
 }
@@ -24,32 +26,28 @@ func (lexer *Lexer) NextToken() token.Token {
 
 	switch lexer.character {
 	case '*':
-		tok = newToken(token.ASTERISK, lexer.character)
+		tok = newToken(token.ASTERISK, string(lexer.character))
 	case ';':
-		tok = newToken(token.SEMICOLON, lexer.character)
+		tok = newToken(token.SEMICOLON, string(lexer.character))
 	case ',':
-		tok = newToken(token.COMMA, lexer.character)
+		tok = newToken(token.COMMA, string(lexer.character))
 	case '(':
-		tok = newToken(token.LPAREN, lexer.character)
+		tok = newToken(token.LPAREN, string(lexer.character))
 	case ')':
-		tok = newToken(token.RPAREN, lexer.character)
+		tok = newToken(token.RPAREN, string(lexer.character))
 	case '\'':
-		tok = newToken(token.APOSTROPHE, lexer.character)
+		lexer.insideApostrophes = !lexer.insideApostrophes
+		tok = newToken(token.APOSTROPHE, string(lexer.character))
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
 	default:
-		if isDigit(lexer.character) {
-			tok.Type = token.LITERAL
-			tok.Literal = lexer.readNumber()
-			return tok
-		} else if isLetter(lexer.character) {
-			tok.Literal = lexer.readIdentifier()
-			tok.Type = token.LookupIdent(tok.Literal)
-			return tok
+		if lexer.insideApostrophes {
+			characters := lexer.processCharacters([]byte{'\''}, []byte{' ', '\n', '\t', '\r'})
+			return characters
 		} else {
-			// unsupported stuff
-			tok = newToken(token.ILLEGAL, lexer.character)
+			characters := lexer.processCharacters([]byte{'\'', '(', ',', ';', '*', ')'}, []byte{})
+			return characters
 		}
 	}
 
@@ -58,35 +56,61 @@ func (lexer *Lexer) NextToken() token.Token {
 }
 
 func (lexer *Lexer) skipWhitespace() {
-	for lexer.character == ' ' || lexer.character == '\t' || lexer.character == '\n' || lexer.character == '\r' {
+	for isWhitespace(lexer.character) && !lexer.insideApostrophes {
 		lexer.readChar()
 	}
 }
 
 func (lexer *Lexer) readChar() {
-	if lexer.readPosition >= len(lexer.input) {
-		lexer.character = 0
-	} else {
-		lexer.character = lexer.input[lexer.readPosition]
-	}
+	lexer.character = lexer.getNextChar()
 	lexer.position = lexer.readPosition
 	lexer.readPosition += 1
 }
 
-func (lexer *Lexer) readIdentifier() string {
-	position := lexer.position
-	for isLetter(lexer.character) || isDigit(lexer.character) {
-		lexer.readChar()
+func (lexer *Lexer) getNextChar() byte {
+	if lexer.readPosition >= len(lexer.input) {
+		return 0
 	}
-	return lexer.input[position:lexer.position]
+	return lexer.input[lexer.readPosition]
 }
 
-func (lexer *Lexer) readNumber() string {
+func (lexer *Lexer) processCharacters(blacklist []byte, whitelist []byte) token.Token {
+	nextChar := lexer.getNextChar()
 	position := lexer.position
-	for isDigit(lexer.character) {
+
+	hasDigit := isDigit(lexer.character)
+	hasLetter := isLetter(lexer.character)
+
+	for validChar(nextChar, whitelist) && !bytes.ContainsAny(blacklist, string(nextChar)) {
 		lexer.readChar()
+		nextChar = lexer.getNextChar()
+
+		hasDigit = hasDigit || isDigit(lexer.character)
+		hasLetter = hasLetter || isLetter(lexer.character)
 	}
-	return lexer.input[position:lexer.position]
+	lexer.readChar()
+
+	return lexer.evaluateToken(position, hasLetter, hasDigit)
+}
+
+func (lexer *Lexer) evaluateToken(position int, hasLetter bool, hasDigit bool) token.Token {
+	characters := lexer.input[position:lexer.position]
+
+	if (hasLetter && hasDigit) || lexer.insideApostrophes {
+		return newToken(token.IDENT, characters)
+	}
+	if hasLetter && !hasDigit {
+		return newToken(token.LookupIdent(characters), characters)
+	}
+	if !hasLetter && hasDigit {
+		return newToken(token.LITERAL, characters)
+	}
+
+	return newToken(token.ILLEGAL, characters)
+}
+
+func validChar(nextChar byte, whitelist []byte) bool {
+	return '!' <= nextChar && nextChar <= '~' || bytes.ContainsAny(whitelist, string(nextChar))
 }
 
 func isLetter(ch byte) bool {
@@ -98,9 +122,13 @@ func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
 
-func newToken(tokenType token.Type, ch byte) token.Token {
+func isWhitespace(ch byte) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
+}
+
+func newToken(tokenType token.Type, characters string) token.Token {
 	return token.Token{
 		Type:    tokenType,
-		Literal: string(ch),
+		Literal: characters,
 	}
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -9,8 +9,8 @@ import (
 func TestLexer(t *testing.T) {
 	input :=
 		`
-			CREATE TABLE 	tbl( one TEXT , two INT );
-			INSERT INTO tbl VALUES( 'hello',	 10 );
+			CREATE TABLE 	1tbl( one TEXT , two INT );
+			INSERT INTO tbl VALUES( 'CREATE',	 10 );
 			INSERT 	INTO tbl  VALUES( 'goodbye', 20 );
 			`
 	tests := []struct {
@@ -19,7 +19,7 @@ func TestLexer(t *testing.T) {
 	}{
 		{token.CREATE, "CREATE"},
 		{token.TABLE, "TABLE"},
-		{token.IDENT, "tbl"},
+		{token.IDENT, "1tbl"},
 		{token.LPAREN, "("},
 		{token.IDENT, "one"},
 		{token.TEXT, "TEXT"},
@@ -34,7 +34,7 @@ func TestLexer(t *testing.T) {
 		{token.VALUES, "VALUES"},
 		{token.LPAREN, "("},
 		{token.APOSTROPHE, "'"},
-		{token.IDENT, "hello"},
+		{token.IDENT, "CREATE"},
 		{token.APOSTROPHE, "'"},
 		{token.COMMA, ","},
 		{token.LITERAL, "10"},
@@ -113,6 +113,72 @@ func TestLexerWithNumbersMixedInLitterals(t *testing.T) {
 		{token.LPAREN, "("},
 		{token.APOSTROPHE, "'"},
 		{token.IDENT, "good123bye"},
+		{token.APOSTROPHE, "'"},
+		{token.COMMA, ","},
+		{token.LITERAL, "20"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := RunLexer(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestLexerWithNumbersWithWhitespacesIdentifier(t *testing.T) {
+	input :=
+		`
+			CREATE TABLE 	tbl2( one TEXT , two INT );
+			INSERT INTO tbl2 VALUES( ' hello1',	 10 );
+			INSERT 	INTO tbl2  VALUES( 'Hello	 Dear', 20 );
+			`
+	tests := []struct {
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{token.CREATE, "CREATE"},
+		{token.TABLE, "TABLE"},
+		{token.IDENT, "tbl2"},
+		{token.LPAREN, "("},
+		{token.IDENT, "one"},
+		{token.TEXT, "TEXT"},
+		{token.COMMA, ","},
+		{token.IDENT, "two"},
+		{token.INT, "INT"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.INSERT, "INSERT"},
+		{token.INTO, "INTO"},
+		{token.IDENT, "tbl2"},
+		{token.VALUES, "VALUES"},
+		{token.LPAREN, "("},
+		{token.APOSTROPHE, "'"},
+		{token.IDENT, " hello1"},
+		{token.APOSTROPHE, "'"},
+		{token.COMMA, ","},
+		{token.LITERAL, "10"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.INSERT, "INSERT"},
+		{token.INTO, "INTO"},
+		{token.IDENT, "tbl2"},
+		{token.VALUES, "VALUES"},
+		{token.LPAREN, "("},
+		{token.APOSTROPHE, "'"},
+		{token.IDENT, "Hello\t Dear"},
 		{token.APOSTROPHE, "'"},
 		{token.COMMA, ","},
 		{token.LITERAL, "20"},
